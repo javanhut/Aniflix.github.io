0c650e1abf51df3b2c9664a163fb99bda21db5c1
Javan H. <javanstorm@gmail.com>
2024-04-14 23:46:41 -0400

    fixed issue with carousel

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -1,10 +1,9 @@
 "use client";
-import { Image } from "@nextui-org/react";
+import { Card, CardBody, CardHeader } from "@nextui-org/react";
 import AniflixNav from "@/components/navbar";
-import { Carousel, CarouselItem, Container, Row } from "react-bootstrap";
-import { mashle, one_piece_banner, sl_banner, tsukimichi } from "@/images/images";
+import { kaiju_n8, mashle, one_piece_banner, sl_banner, tsukimichi } from "@/images/images";
 import CarouselBanner from "@/components/carouselBanner";
-import kaiju_n8 from "@/images/kaiju_n8.jpg"
+import Image from "next/image";
 
 function Home() {
     const home_nav_items = [
@@ -14,22 +13,28 @@ function Home() {
         { name: "Recently Watched", link: "recently-watched", styling: "", lstyle: "" },
     ];
     const images_carousel_items = [
-        {src: kaiju_n8, caption: "Kaiju no.8", description:""},
-        {src: mashle, caption: "Mashle Magic and Muscles", description:""},
-        {src: one_piece_banner, caption: "One Piece", description:""},
-        {src: sl_banner, caption: "Solo Leveling", description:""},
-        {src: tsukimichi, caption: "Tsukimichi Moonlight Fantasy", description:""},
+        { src: kaiju_n8, caption: "Kaiju no.8", description: "" },
+        { src: mashle, caption: "Mashle Magic and Muscles", description: "" },
+        { src: one_piece_banner, caption: "One Piece", description: "" },
+        { src: sl_banner, caption: "Solo Leveling", description: "" },
+        { src: tsukimichi, caption: "Tsukimichi Moonlight Fantasy", description: "" },
     ];
     return (<>
         <div className="relative bg-midnight-black bg-cover w-screen h-screen">
             <div className="relative bg-electric-blue">
                 <AniflixNav navbarItems={home_nav_items} />
             </div>
-            <Container>
-                <Row>
-                <CarouselBanner banner_items={images_carousel_items}/>
-                </Row>
-            </Container>
+            <CarouselBanner banner_items={images_carousel_items} />
+            <div>
+                <Card>
+                    <CardHeader className=" relative bg-eletric-blue  w-full h-full text-center bg-cover">
+                        <div className="text-4xl flex text-center font-bold">Most Popular</div>
+                    </CardHeader>
+                    <CardBody>
+                        <Image />
+                    </CardBody>
+                </Card>
+            </div>
         </div>
     </>);
 }
--- src/components/carouselBanner.js
+++ src/components/carouselBanner.js
@@ -1,21 +1,20 @@
 "use client";
 import { Carousel, CarouselItem, CarouselCaption } from "react-bootstrap";
 import Image from "next/image";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 export default function CarouselBanner({ banner_items }) {
-    const items = banner_items;
     return (
-    <>
         <Carousel>
-            {items.map((item, index) => (
+            {banner_items.map((item, index) => (
                 <CarouselItem key={index}>
-                    <Image src={item.src}/>
+                    <Image className="w-screen h-[500px] object-cover" src={item.src} alt={item.caption} placeholder="blur" />
                     <CarouselCaption>
                         <h3>{item.caption}</h3>
                         <p>{item.description}</p>
-                        </CarouselCaption>
+                    </CarouselCaption>
                 </CarouselItem>
             ))}
         </Carousel>
-    </>)
-    ;
+    );
 }
